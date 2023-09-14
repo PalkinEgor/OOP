e337c4cb68d154710712ee568b05783f8520956b
PalkinEgor <palkin122004@gmail.com>
2023-09-14 22:45:56 +0700

    change code style again

--- Task_1_1_1/src/main/java/ru/nsu/palkin/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/palkin/Main.java
@@ -1,39 +1,40 @@
 package ru.nsu.palkin;
 
 public class Main {
-  public static void heapify(int[] arr, int len, int i) {
-    int largestIndex = i;
-    int leftChildIndex = i * 2 + 1;
-    int rightChildIndex = i * 2 + 2;
-    if (leftChildIndex < len && arr[leftChildIndex] > arr[largestIndex]) {
-      largestIndex = leftChildIndex;
-    }
-    if (rightChildIndex < len && arr[rightChildIndex] > arr[largestIndex]) {
-      largestIndex = rightChildIndex;
-    }
-    if (largestIndex != i) {
-      int tmp = arr[largestIndex];
-      arr[largestIndex] = arr[i];
-      arr[i] = tmp;
+    public static void heapify(int[] arr, int len, int i) {
+        int largestIndex = i;
+        int leftChildIndex = i * 2 + 1;
+        int rightChildIndex = i * 2 + 2;
+        if (leftChildIndex < len && arr[leftChildIndex] > arr[largestIndex]) {
+            largestIndex = leftChildIndex;
+        }
+        if (rightChildIndex < len && arr[rightChildIndex] > arr[largestIndex]) {
+            largestIndex = rightChildIndex;
+        }
+        if (largestIndex != i) {
+            int tmp = arr[largestIndex];
+            arr[largestIndex] = arr[i];
+            arr[i] = tmp;
 
-      heapify(arr, len, largestIndex);
+            heapify(arr, len, largestIndex);
+        }
     }
-  }
 
-  public static int[] heapsort(int[] arr) {
-    int len = arr.length;
-    for (int i = len / 2 - 1; i >= 0; i--) {
-      heapify(arr, len, i);
+    public static int[] heapsort(int[] arr) {
+        int len = arr.length;
+        for (int i = len / 2 - 1; i >= 0; i--) {
+            heapify(arr, len, i);
+        }
+        for (int i = 0; i < len; i++) {
+            int tmp = arr[0];
+            arr[0] = arr[len - 1 - i];
+            arr[len - 1 - i] = tmp;
+
+            heapify(arr, len - 1 - i, 0);
+        }
+        return arr;
     }
-    for (int i = 0; i < len; i++) {
-      int tmp = arr[0];
-      arr[0] = arr[len - 1 - i];
-      arr[len - 1 - i] = tmp;
 
-      heapify(arr, len - 1 - i, 0);
+    public static void main(String[] args) {
     }
-    return arr;
-  }
-
-  public static void main(String[] args) {}
 }
--- Task_1_1_1/src/test/java/ru/nsu/palkin/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/palkin/MainTest.java
@@ -5,38 +5,33 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class MainTest {
-  @Test
-  public void simpleHeapsortTest() {
-    assertArrayEquals(new int[] {1, 2, 3, 4, 5}, Main.heapsort(new int[] {3, 2, 5, 1, 4}));
-  }
+    @Test
+    public void simpleHeapsortTest() {
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, Main.heapsort(new int[]{3, 2, 5, 1, 4}));
+    }
 
-  @Test
-  public void repeatedValuesTest() {
-    assertArrayEquals(new int[] {0, 0, 0, 1, 1}, Main.heapsort(new int[] {0, 1, 0, 1, 0}));
-  }
+    @Test
+    public void repeatedValuesTest() {
+        assertArrayEquals(new int[]{0, 0, 0, 1, 1}, Main.heapsort(new int[]{0, 1, 0, 1, 0}));
+    }
 
-  @Test
-  public void emptyArrayTest() {
-    assertArrayEquals(new int[] {}, Main.heapsort(new int[] {}));
-  }
+    @Test
+    public void emptyArrayTest() {
+        assertArrayEquals(new int[]{}, Main.heapsort(new int[]{}));
+    }
 
-  @Test
-  public void negativeValuesTest() {
-    assertArrayEquals(new int[] {-2, -1, 0, 1, 2}, Main.heapsort(new int[] {0, 1, -2, -1, 2}));
-  }
+    @Test
+    public void negativeValuesTest() {
+        assertArrayEquals(new int[]{-2, -1, 0, 1, 2}, Main.heapsort(new int[]{0, 1, -2, -1, 2}));
+    }
 
-  @Test
-  public void sortedArrayTest() {
-    assertArrayEquals(new int[] {1, 2, 3, 4, 5}, Main.heapsort(new int[] {1, 2, 3, 4, 5}));
-  }
+    @Test
+    public void sortedArrayTest() {
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, Main.heapsort(new int[]{1, 2, 3, 4, 5}));
+    }
 
-  @Test
-  public void longArrayTest() {
-    assertArrayEquals(
-        new int[] {1, 14, 16, 17, 19, 22, 27, 34, 42, 43, 50, 52, 55, 58, 59, 73, 75, 75, 80, 85},
-        Main.heapsort(
-            new int[] {
-              75, 80, 55, 59, 73, 17, 34, 27, 22, 85, 14, 43, 52, 19, 42, 1, 75, 58, 50, 16
-            }));
-  }
+    @Test
+    public void longArrayTest() {
+        assertArrayEquals(new int[]{1, 14, 16, 17, 19, 22, 27, 34, 42, 43, 50, 52, 55, 58, 59, 73, 75, 75, 80, 85}, Main.heapsort(new int[]{75, 80, 55, 59, 73, 17, 34, 27, 22, 85, 14, 43, 52, 19, 42, 1, 75, 58, 50, 16}));
+    }
 }
