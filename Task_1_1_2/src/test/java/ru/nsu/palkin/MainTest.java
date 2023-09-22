7862b0a324dc5664de5228268dabe97006392aa2
PalkinEgor <palkin122004@gmail.com>
2023-09-22 14:10:06 +0700

    second lab

---
+++ Task_1_1_2/src/test/java/ru/nsu/palkin/MainTest.java
@@ -0,0 +1,54 @@
+package ru.nsu.palkin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    @Test
+    public void plusTest() {
+        Polynomial p1 = new Polynomial(new int[]{4, 3, 6, 7});
+        Polynomial p2 = new Polynomial(new int[]{3, 2, 8});
+        assertEquals("7.0x^3 + 14.0x^2 + 5.0x + 7.0", p1.plus(p2).toStr());
+    }
+
+    @Test
+    public void minusTest() {
+        Polynomial p1 = new Polynomial(new double[]{4, 3, 6, 7});
+        Polynomial p2 = new Polynomial(new int[]{3, 2, 8});
+        assertEquals("7.0x^3 - 2.0x^2 + x + 1.0", p1.minus(p2).toStr());
+    }
+
+    @Test
+    public void timesTest() {
+        Polynomial p1 = new Polynomial(new int[]{4, 3, 6, 7});
+        Polynomial p2 = new Polynomial(new int[]{3, 2, 8});
+        assertEquals("56.0x^5 + 62.0x^4 + 57.0x^3 + 56.0x^2 + 17.0x + 12.0", p1.times(p2).toStr());
+    }
+
+    @Test
+    public void differentiateTest() {
+        Polynomial p1 = new Polynomial(new int[]{4, 3, 6, 7});
+        assertEquals("21.0x^2 + 12.0x + 3.0", p1.differentiate(1).toStr());
+    }
+
+    @Test
+    public void evaluateTest() {
+        Polynomial p1 = new Polynomial(new int[]{4, 3, 6, 7});
+        assertEquals(90, p1.evaluate(2));
+    }
+
+    @Test
+    public void equalityTrueTest() {
+        Polynomial p1 = new Polynomial(new int[]{4, 3, 6, 7});
+        Polynomial p2 = new Polynomial(new int[]{4, 3, 6, 7});
+        assertTrue(p1.equality(p2));
+    }
+
+    @Test
+    public void equalityFalseTest() {
+        Polynomial p1 = new Polynomial(new int[]{4, 3, 6, 7});
+        Polynomial p2 = new Polynomial(new int[]{1, 2, 3});
+        assertFalse(p1.equality(p2));
+    }
+}
