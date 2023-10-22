69690f923305269f54b4f83060d5e39b27d8e4f2
PalkinEgor <PalkinEgor@users.noreply.github.com>
2023-10-22 08:25:18 +0000

    Deploying to gh-pages from @ PalkinEgor/OOP@a9427bc8c9f856027105a8bbe99bf344890a84cc 🚀

---
+++ Task_1_2_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.palkin","l":"Edge"},{"p":"ru.nsu.palkin","l":"Graphable"},{"p":"ru.nsu.palkin","l":"GraphAdjacencyList"},{"p":"ru.nsu.palkin","l":"GraphAdjacencyMatrix"},{"p":"ru.nsu.palkin","l":"GraphIncidenceMatrix"},{"p":"ru.nsu.palkin","l":"Main"}]
