04baf6086bea57328b3a7bba35396cd745c43908
PalkinEgor <palkin122004@gmail.com>
2024-02-17 18:29:16 +0700

    review dog warnings

--- Task_2_1_1/src/main/java/ru/nsu/palkin/Prime.java
+++ Task_2_1_1/src/main/java/ru/nsu/palkin/Prime.java
@@ -1,6 +1,15 @@
 package ru.nsu.palkin;
 
+/**
+ * Prime interface.
+ */
 public interface Prime {
+    /**
+     * Default method that check number is prime or not.
+     *
+     * @param x - number
+     * @return true or false
+     */
     default boolean isNotPrime(int x) {
         int sqrt = (int) Math.sqrt(x);
         for (int i = 2; i <= sqrt; i++) {
@@ -11,5 +20,10 @@ public interface Prime {
         return false;
     }
 
+    /**
+     * Method that check is any number not prime.
+     *
+     * @return true or false
+     */
     public boolean hasNotPrime() throws InterruptedException;
 }
